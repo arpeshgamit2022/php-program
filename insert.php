3e37a23e17eed71ea2d4b4c53cb7ad2283de9aad
Arpesh Gamit <108983564+arpeshgamit2022@users.noreply.github.com>
2022-09-26 18:01:11 +0530

    Correction of code

--- insert.php
+++ insert.php
@@ -1,11 +1,12 @@
 <?php
-
+    //include connection
     include "connection.php";
+    // Start session
     session_start();
-
+    //Creating Empty Array
     $validateErrors = array();
 
-    if( isset($_POST['submit']) )
+    if ( isset( $_POST['submit'] ) )
     {
         echo "<pre>";
         print_r($_POST);
@@ -13,76 +14,97 @@
         echo "<pre>";
         print_r($_FILES);
         echo "</pre>";
-        //firstname Validation
+        //firstname Empty Validation
         if ( empty ( $_POST['firstname'] ) )
         {
+            //Store error message in array
             $validateErrors['firstname_error'] = "Firstname is required";
         }
         else
         {
-            $fname = htmlspecialchars( stripslashes( trim( $_POST['firstname'] ) ) );
+            //Remove whitespaces, slashes and store data
+            $fname = stripslashes( trim( $_POST['firstname'] ) );
         }
-        //Lastname Validation
+
+        //Lastname Empty Validation
         if ( empty ( $_POST['lastname'] ) )
         {
+            // Store error message in array
             $validateErrors['lastname_error'] = "Lastname is required";
         }
         else
         {
-            $lname = htmlspecialchars( stripslashes( trim( $_POST['lastname'] ) ) );
+            //Remove whitespaces, slashes and store data
+            $lname = stripslashes( trim( $_POST['lastname'] ) );
         }
-        //Email Validation
+
+        //Email Empty Validation
         if ( empty ( $_POST['email'] ) )
         {
+            //Store error message
             $validateErrors['email_error'] = "Email is required";
         }
         else
         {
-            $email = htmlspecialchars( stripslashes( trim( $_POST['email'] ) ) );
+            //Remove whitespaces, slashes and store data in a variable
+            $email = stripslashes( trim( $_POST['email'] ) );
             
+            //Check if email is well-formed
             if(!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email))
             {
+                //Store error message
                 $validateErrors['email_error']="Please enter correct email";
             }
         }
-        //Phone Validation
+
+        //Phone Empty Validation
         if ( empty ( $_POST['phone'] ) )
         {
+            //Store error message
             $validateErrors['phone_error'] = "Phone is required";
         }
         else
         {
-            $phone = htmlspecialchars( stripslashes( trim( $_POST['phone'] ) ) );
+            //Remove whitespaces, slashes and store data in a variable
+            $phone = stripslashes( trim( $_POST['phone'] ) );
             
             if ( !preg_match('/^[0-9]{10}+$/', $phone) )
             {
+                //store error message
                 $validateErrors['phone_error']="Please enter correct phone";
             }
         }
-        //Gender Validation
+
+        //Gender Empty Validation
         if ( empty ( $_POST['gender'] ) || !isset ( $_POST['gender'] ) )
         {
+            //store error message
             $validateErrors['gender_error'] = "Gender is required";
         }
         else
         {
+            //Store data in variable
             $gender = $_POST['gender'];
         }
-        //Birthdate Validation
+        //Birthdate Empty Validation
         if ( empty ( $_POST['birthdate'] ) )
         {
+            //Store error message
             $validateErrors['birthdate_error'] = "Birthdate is required";
         }
         else
         {
-            $birthdate = htmlspecialchars( stripslashes( trim( $_POST['birthdate'] ) ) );
-            //today's date
-            $currentDate = date( "d-m-Y" );
-
-            $entrantAge = new DateTime( $birthdate );
-            $currentDate = new DateTime( $currentDate );
-
-            $interval = $entrantAge->diff( $currentDate );
+            $birthdate = trim( $_POST['birthdate'] );
+            //Convert birthdate in dd/mm/yy format
+            $result=explode('-',$birthdate);
+            $date=$result[2];
+            $month=$result[1];
+            $year=$result[0];
+            $userDate=$date.'-'.$month.'-'.$year;
+            // echo $userDate;
+            $currentDate = new DateTime('today');
+            // echo $currentDate;
+            $interval = $currentDate->diff($userDate);
             $myage = $interval->y;
 
             if ( $myage >= 18 )
@@ -90,53 +112,56 @@
                 $validateErrors['birthdate_error'] = "Please enter DOB above 18 Years";
             }
         }
-        //Qualification Validation
+        //Qualification Empty Validation
         if ( empty ( $_POST['qualification'] ) || !isset ( $_POST['qualification'] ) )
         {
+            //Store error message
             $validateErrors['qualification_error'] = "Qualification is required";
         }
         else
         {
             $qualification = $_POST['qualification'];
+            //Return string from an array using implode function
             $qua = implode (',', $qualification);
         }
         //Photo Validation
+        //check whether Variable is set or not
         if ( isset( $_FILES['photo'] ) )
         {
             $file_name = $_FILES['photo']['name'];
             $file_size = $_FILES['photo']['size'];
             $file_tmp = $_FILES['photo']['tmp_name'];
             $file_type = $_FILES['photo']['type'];
+            //Store image type like png,jpeg
             $file_ext = strtolower( end( explode( '.', $_FILES['photo']['name'] ) ) );
             
             $extensions = array( "jpeg","jpg","png" ); 
-            
+            //Match file type both are same or not
             if ( in_array( $file_ext,$extensions ) === false)
             {
                $validateErrors['file_allowed'] = "extension not allowed, please choose a JPEG or PNG file.";
             }
-            
+            //Match file size with 2097152 
             if ( $file_size > 2097152 )  
             {
                $validateErrors['file_size'] = 'File size must be excately 2 MB';
             }
-            
+            //Condition true then store in uploads file
             if ( empty( $validateErrors ) == true) 
             { 
                move_uploaded_file( $file_tmp,"uploads/".$file_name );
-
             }
-
-            if (file_exists($file_ext)) 
+            //Check file exists or not
+            if (file_exists($file_name)) 
             {
                 $validateErrors['file_error'] = 'File is already exists';
             } 
-
             else
             {
                print_r( $validateErrors );
             }
         }
+        
         //Description Validation
         if ( empty ( $_POST['description'] ) )
         {
@@ -144,9 +169,9 @@
         }
         else
         {
-            $description = htmlspecialchars( stripslashes( trim( $_POST['description'] ) ) );
+            $description = stripslashes( trim( $_POST['description'] ) );
         }
-
+        //If not empty then store in session
         if(!empty($validateErrors))
         {
             $_SESSION['errors_data'] = $validateErrors;
@@ -158,25 +183,25 @@
             $_SESSION['gender'] = $gender;
             $_SESSION['birthdate'] = $birthdate;
             $_SESSION['qualification'] = $qualification;
-            $_SESSION['file_name'] = $file_ext;
+            $_SESSION['file_name'] = $file_name;
             $_SESSION['description'] = $description;
-
-            header("Location: /arpesh/employee-database-practical/addEmployee.php");
+            //Redirect to this page
+            // header("Location: /arpesh/employee-database-practical/addEmployee.php");
         }
         else
         {
+            //Insert data in the database using insert query
             $insertQuery = "INSERT INTO emp (firstname, lastname, email, phone, gender, birthdate, qualification, photo, description) VALUES (?,?,?,?,?,?,?,?,?)";
             $stmt= $pdo->prepare($insertQuery);
-            $stmt->execute([$fname, $lname, $email, $phone, $gender, $birthdate, $qua, $file_ext , $description]);
+            $stmt->execute([$fname, $lname, $email, $phone, $gender, $birthdate, $qua, $file_name , $description]);
             
-            header("Location: /arpesh/employee-database-practical/addEmployee.php");
-
-            if($stmt)
-            {
-                $_SESSION['insert_data'] = "Data Successfully Inserted";
-            }
+            // if($stmt)
+            // {
+            //     $_SESSION['insert_data'] = "Data Successfully Inserted";
+            // }
             
-            // $validateErrors['insert_data'] = "Data Successfully Inserted";
+            //If successfully inserted data then redirect to this page
+            // header("Location: /arpesh/employee-database-practical/index.php");
         }
     }
 
